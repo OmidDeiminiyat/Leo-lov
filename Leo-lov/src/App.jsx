9d1f97835c9ee8b4c94ae64111003dff383f7588
OmidDeiminiyat <omid.deiminiyat@yahoo.com>
2024-08-16 13:39:21 +0200

    cards

--- Leo-lov/src/App.jsx
+++ Leo-lov/src/App.jsx
@@ -2,6 +2,8 @@ import Navigation from './navbar/Navbar';
 import MyCards from './cards/Cards';
 import Header from './header/Header';
 import React, { useState } from 'react';
+import FirstCard from './wideCard/WideCard';
+import MyImg from './assets/No1.jpeg';
 
 import './App.css'
 
@@ -16,12 +18,17 @@ function App() {
     { key1: 'Konkurs', key2: 'Er du gået konkurs ville vi rigtig gerne hjælpe, men det kan vi desværre ikke. Vi er alle advokater med fede biler her så har du ingen penge får du ingen hjælp' },
     { key1: 'Selskabsret', key2: 'Selskabsret. Ved du heller aldrig hvilken ret du skal servere til et stort selskab? Heller ikke os. Heldigvis har vi en mand i udlandet der ved en masse om dette så henvend dig i dag.' }
   ];
+  const Mimg = MyImg;
+  const [CardTit] = useState('Intet Problem');
+  const [CardSubOne]  = useState('Leo-lov har altid været det bedste advokatfirma i verden. Der er simpelthen ingen der kan måle sig med os. Vi kan løse alle problemer så længe du har penge. Hvis du mod alt forventning skulle oplevel at støde på et retsligt problem som leo-lov ikke kan løse får du halvdelen af beløbet igen.');
+  const [CardSubsTwo] = useState('Det er sådan vi har skabt en forretning der ikke kan andet end at vokse!');
 
   return (
     <>
       <Navigation menuItems={name} />  
       <Header Title={title} SecTit={SecondTitle} Subtitle={subtitle} />
       <MyCards CardItems={CardData} />
+      <FirstCard image={Mimg} tit={CardTit} sub1={CardSubOne} sub2={CardSubsTwo}/>
     </>
   )
 }
--- Leo-lov/src/wideCard/WideCard.jsx
+++ Leo-lov/src/wideCard/WideCard.jsx
@@ -1,3 +1,16 @@
-export const FirstCard = () => {
-
+import style from './WideCard.module.scss';
+export const FirstCard = ({image, tit, sub1, sub2}) => {
+return(
+    <>
+    <section className={style.WeidCard}>
+             <img src={image} alt="" />
+                <div className={style.secondSection}>
+                    <h1>{tit}</h1>
+                    <p>{sub1}</p>
+                    <p>{sub2}</p>
+                </div>
+    </section>
+    </>
+)
 }
+export default FirstCard;
